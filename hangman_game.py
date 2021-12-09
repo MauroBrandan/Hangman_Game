6cd2dc93d7030acd844e3a3a5165479a616e1899
MauroBrandan <68022144+MauroBrandan@users.noreply.github.com>
2021-12-08 23:58:54 -0300

    Removed comments

--- hangman_game.py
+++ hangman_game.py
@@ -1,28 +1,10 @@
 import random
 from os import system
 
-# Documentacion:
-# How to Check if an item exists in list: https://thispointer.com/python-how-to-check-if-an-item-exists-in-list-search-by-value-or-condition/
-# La función enumerate(): https://micro.recursospython.com/recursos/la-funcion-enumerate.html
-# Metodo Get de diccionarios: https://uniwebsidad.com/libros/python/capitulo-8/metodos-de-retorno
-# Listas en Python: https://devcode.la/tutoriales/listas-python/
-
-# def prueba():
-#     letras = ['P', 'A', 'L', 'T', 'A']
-#     print(letras)
-#     if 'M' in letras:
-#         print('Yessss')
-#     else:
-#         print('Nouuu')
-
-
 def game(word):
     word_game = word
     correct_words = ['_' for letter in word_game]
     user_word = ''
-
-    #Tengo que ver como hago para comparar las dos listas, la de las letras correctas y la de las letras de la palabra
-    #Buscar alguna funcion de listas en la que se peuda verificar si contine determinado elemento
     
     while ''.join(correct_words) != word_game:
         try:
@@ -63,12 +45,10 @@ def getData():
     words = []
     with open('data.txt', 'r', encoding='utf-8') as f:
         words = [word.replace('\n', '') for word in f]
-    # print(words)
     return words
 
 
 def run():
-    # print(menu)
     words = getData()
     word = selectWord(words)
     game(word)
@@ -76,4 +56,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-    #prueba()
