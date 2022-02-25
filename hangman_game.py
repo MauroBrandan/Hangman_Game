3d4b3e1956c1b79588ea2549169b20d6177b023f
MauroBrandan <maurobrandan.mb@gmail.com>
2022-02-25 14:43:15 -0300

    Add lives

--- hangman_game.py
+++ hangman_game.py
@@ -1,41 +1,29 @@
 import random
 from os import system
 
-# Documentacion:
-# How to Check if an item exists in list: https://thispointer.com/python-how-to-check-if-an-item-exists-in-list-search-by-value-or-condition/
-# La función enumerate(): https://micro.recursospython.com/recursos/la-funcion-enumerate.html
-# Metodo Get de diccionarios: https://uniwebsidad.com/libros/python/capitulo-8/metodos-de-retorno
-# Listas en Python: https://devcode.la/tutoriales/listas-python/
-
-# def prueba():
-#     letras = ['P', 'A', 'L', 'T', 'A']
-#     print(letras)
-#     if 'M' in letras:
-#         print('Yessss')
-#     else:
-#         print('Nouuu')
-
-
 def game(word):
     word_game = word
     correct_words = ['_' for letter in word_game]
     user_word = ''
+    lives = 5
 
-    #Tengo que ver como hago para comparar las dos listas, la de las letras correctas y la de las letras de la palabra
-    #Buscar alguna funcion de listas en la que se peuda verificar si contine determinado elemento
-    
     while ''.join(correct_words) != word_game:
         try:
-            print('Adivina la palabra')
+            print(f'Adivina la palabra (Vidas: {lives})')
             print(' '.join(correct_words))
             user_word = input('Ingresa una letra: ').upper()
         
             if len(user_word) > 1 or user_word.isdigit():
                 raise ValueError
 
+            previous_correct_words = ''.join(correct_words)
+
             for i, letra in enumerate(word_game):
                 if word_game[i] == user_word:
                     correct_words[i] = user_word
+            
+            if previous_correct_words == ''.join(correct_words):
+                lives -= 1
             
             system("cls")
         
@@ -63,12 +51,10 @@ def getData():
     words = []
     with open('data.txt', 'r', encoding='utf-8') as f:
         words = [word.replace('\n', '') for word in f]
-    # print(words)
     return words
 
 
 def run():
-    # print(menu)
     words = getData()
     word = selectWord(words)
     game(word)
@@ -76,4 +62,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-    #prueba()
